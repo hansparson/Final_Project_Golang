7b7fbc6213f2677aff557e11e161f4d1a611c021
Hans Parson <hans@motionpay.id>
2022-10-16 18:15:40 +0700

    Final Project Golang by Hans Parson

--- docs/docs.go
+++ docs/docs.go
@@ -6,7 +6,7 @@ import "github.com/swaggo/swag"
 
 const docTemplate = `{
     "schemes": {{ marshal .Schemes }},
-    "swagger": "3.0",
+    "swagger": "2.0",
     "info": {
         "description": "{{escape .Description}}",
         "title": "{{.Title}}",
