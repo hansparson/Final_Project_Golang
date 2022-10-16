d5f2f94fa3acddd2cda158eaa9d25ab70b422069
Hans Parson <hans@motionpay.id>
2022-10-16 12:27:15 +0700

    Final Project Golang

--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"final/db"
+	"final/server"
+	"final/server/services"
+	"os"
 )
 
-var PORT = ":8080"
+// @title My Grams APP
+// @description Final Project Golang
+// @version v1.0
+// @termsOfService http://swagger.io/terms/
+// @BasePath /
+// @host localhost:52000
+// @contact.name Hans Parson
+// @contact.email hansparson013@gmail.com
 
 func main() {
-	http.HandleFunc("/", greet)
+	var PORT = os.Getenv("PORT")
+	db := db.ConnectGorm()
 
-	http.ListenAndServe(PORT, nil)
-}
-
-func greet(w http.ResponseWriter, r *http.Request) {
-	msg := "Hello World"
-	fmt.Fprint(w, msg)
+	//// Running Service User ////
+	serviceController := services.User_DB_Controller(db)
+	user_service := server.UserRouther(serviceController)
+	user_service.Start(":" + PORT)
 }
